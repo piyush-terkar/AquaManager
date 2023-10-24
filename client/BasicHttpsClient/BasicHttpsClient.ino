01ece6b306764f42cbc992f7233808486761021f
Piyush Terkar <terkar.piyush3@gmail.com>
2023-10-24 15:04:03 +0530

    ADD: Controller changes for basic http client

---
+++ client/BasicHttpsClient/BasicHttpsClient.ino
@@ -0,0 +1,120 @@
+/**
+   BasicHTTPSClient.ino
+
+    Created on: 20.08.2018
+
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClientSecureBearSSL.h>
+// Fingerprint for demo URL, expires on June 2, 2021, needs to be updated well before this date
+//const uint8_t fingerprint[20] = { 0x40, 0xaf, 0x00, 0x6b, 0xec, 0x90, 0x22, 0x41, 0x8e, 0xa3, 0xad, 0xfa, 0x1a, 0xe8, 0x25, 0x41, 0x1d, 0x1a, 0x54, 0xb3 };
+
+ESP8266WiFiMulti WiFiMulti;
+int filter = D5;
+int light = D6;
+int pump = D7;
+void setup() {
+
+  pinMode(filter, OUTPUT);
+  digitalWrite(filter, HIGH);
+  pinMode(light, OUTPUT);
+  digitalWrite(light, HIGH);
+  pinMode(pump, OUTPUT);
+  digitalWrite(pump, HIGH);
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("TEA@2.4GHz", "insta11wifi");
+}
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+
+//    client->setFingerprint(fingerprint);
+    // Or, if you happy to ignore the SSL certificate, then use the following line instead:
+     client->setInsecure();
+
+    HTTPClient https;
+    StaticJsonDocument<100> doc;
+    if (https.begin(*client, "https://192.168.1.7:3000/sync")) {  // HTTPS
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String message = https.getString();
+//          Serial.println();
+          DeserializationError err = deserializeJson(doc, message);
+    if (err)
+    {
+    }
+    else
+    {
+      Serial.println(message);
+      boolean filterStatus = doc["filter"];
+      boolean lightStatus = doc["light"];
+      boolean pumpStatus = doc["pump"];
+      if (filterStatus)
+      {
+        digitalWrite(filter, LOW);
+      }
+      else
+      {
+        digitalWrite(filter, HIGH);
+      }
+      if (lightStatus)
+      {
+        digitalWrite(light, LOW);
+      }
+      else
+      {
+        digitalWrite(light, HIGH);
+      }
+      if (pumpStatus)
+      {
+        digitalWrite(pump, LOW);
+      }
+      else
+      {
+        digitalWrite(pump, HIGH);
+      }
+    }
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+  }
+  delay(1000);
+}
--- client/WebClient/WebClient.ino
+++ client/WebClient/WebClient.ino
@@ -13,29 +13,30 @@
 // Emulate Serial1 on pins 6/7 if not present
 #ifndef HAVE_HWSERIAL1
 #include "SoftwareSerial.h"
+#include "WiFiClientSecureBearSSL.h"
 SoftwareSerial Serial1(6, 7); // RX, TX
 #endif
 
-char ssid[] = "TEA@2.4GHz";  // your network SSID (name)
-char pass[] = "insta11wifi"; // your network password
-int status = WL_IDLE_STATUS; // the Wifi radio's status
+char ssid[] = "TEA@2.4GHz";            // your network SSID (name)
+char pass[] = "insta11wifi";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
-char server[] = "192.168.1.14";
+char server[] = "192.168.1.7";
 
 // Initialize the Ethernet client object
 WiFiEspClient client;
 int filter = 8;
-int light = 9;
-int pump = 10;
+  int light = 9;
+  int pump = 10;
 void setup()
-{
+{   
   pinMode(filter, OUTPUT);
   digitalWrite(filter, HIGH);
   pinMode(light, OUTPUT);
   digitalWrite(light, HIGH);
   pinMode(pump, OUTPUT);
   digitalWrite(pump, HIGH);
-
+  
   // initialize serial for debugging
   Serial.begin(115200);
   // initialize serial for ESP module
@@ -44,17 +45,14 @@ void setup()
   WiFi.init(&Serial1);
 
   // check for the presence of the shield
-  if (WiFi.status() == WL_NO_SHIELD)
-  {
+  if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     // don't continue
-    while (true)
-      ;
+    while (true);
   }
 
   // attempt to connect to WiFi network
-  while (status != WL_CONNECTED)
-  {
+  while ( status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network
@@ -63,14 +61,13 @@ void setup()
 
   // you're connected now, so print out the data
   Serial.println("You're connected to the network");
-
+  
   printWifiStatus();
 
   Serial.println();
   Serial.println("Starting connection to server...");
   // if you get a connection, report back via serial
-  if (client.connect(server, 3030))
-  {
+  if (client.connect(server, 3030)) {
     Serial.println("Connected to server");
   }
 }
@@ -80,60 +77,33 @@ void loop()
   // if there are incoming bytes available
   // from the server, read them and print them
   StaticJsonDocument<100> doc;
-  while (client.connected())
-  {
-    String message;
-    while (client.available())
-    {
-      char c = client.read();
-      message += c;
-    }
+    String message
     DeserializationError err = deserializeJson(doc, message);
-    if (err)
-    {
-    }
-    else
-    {
+    if(err){
+        
+    }else{
       Serial.println(message);
       boolean filterStatus = doc["filter"];
       boolean lightStatus = doc["light"];
       boolean pumpStatus = doc["pump"];
-      if (filterStatus)
-      {
-        digitalWrite(filter, LOW);
+      if(filterStatus){
+          digitalWrite(filter, LOW);
+        }else{
+          digitalWrite(filter, HIGH);
+        }
+      if(lightStatus){
+          digitalWrite(light, LOW);
+        }else{
+          digitalWrite(light, HIGH);
+        }
+      if(pumpStatus){
+          digitalWrite(pump, LOW);
+        }else{
+          digitalWrite(pump, HIGH);
+        }
       }
-      else
-      {
-        digitalWrite(filter, HIGH);
-      }
-      if (lightStatus)
-      {
-        digitalWrite(light, LOW);
-      }
-      else
-      {
-        digitalWrite(light, HIGH);
-      }
-      if (pumpStatus)
-      {
-        digitalWrite(pump, LOW);
-      }
-      else
-      {
-        digitalWrite(pump, HIGH);
-      }
-    }
-  }
-
-  // if the server's disconnected, stop the client
-  while (!client.connected())
-  {
-    Serial.print("Attempting to reconnect...");
-    client.connect(server, 3030);
-    delay(1000);
-    // do nothing forevermore
-  }
 }
+
 
 void printWifiStatus()
 {
